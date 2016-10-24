564cd7d1ea7761dd8c482f6abf2623d426aada9a
Hafizur Rahman <hafuzur.rahmanc@gmail.com>
2016-10-24 12:27:27 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,21 @@
 'use strict';
+function addIngredient(recipe, ingredientName, amounts){
+  recipe[ingredientName] = amounts;
+  return recipe;
+}
+
+function removeIngredient(recipe, ingredientName){
+  delete recipe[ingredientName];
+  return recipe;
+}
+
+function updateIngredient(recipe, ingredientName, amounts){
+  recipe[ingredientName] = amounts;
+  return recipe;
+}
+
+function readRecipe(recipe){
+  for (var key in recipe){
+    console.log("this recipe calls for " + recipe[key] + " of " + key);
+  }
+}
